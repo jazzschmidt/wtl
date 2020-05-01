b862053fc3c663fc8f7ea78334f58eadffd60e67
Carsten Schmidt <jazzschmidt@icloud.com>
2020-05-01 15:59:33 +0200

    Adds sample test suite

--- test.c
+++ test.c
@@ -1,9 +1,34 @@
 #include <stdio.h>
-#include "lib/munit/munit.h"
+#include "test.h"
 
-int main(int argc, const char** argv) {
-  printf("Started tests!\n");
+static MunitTest tests[] = {
+  {
+    "/sample-test", /* name */
+    sample_test, /* test */
+    NULL, /* setup */
+    NULL, /* tear_down */
+    MUNIT_TEST_OPTION_NONE, /* options */
+    NULL /* parameters */
+  },
+  /* Mark the end of the array with an entry where the test
+   * function is NULL */
+  { NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
+};
+
+static const MunitSuite suite = {
+  "/wtl-tests", /* name */
+  tests, /* tests */
+  NULL, /* suites */
+  1, /* iterations */
+  MUNIT_SUITE_OPTION_NONE /* options */
+};
+
+MunitResult sample_test(const MunitParameter params[], void* user_data_or_fuxture) {
   munit_assert_int(42, ==, 42);
 
-  return 0;
+  return MUNIT_OK;
+}
+
+int main(int argc, char* argv[]) {
+  return munit_suite_main(&suite, NULL, argc, argv);
 }
