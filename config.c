d82952ebccdb598e2d67b1053ddb6246b8789055
Carsten Schmidt <jazzschmidt@icloud.com>
2020-05-05 00:16:14 +0200

    Removes log output

--- config.c
+++ config.c
@@ -10,11 +10,6 @@ typedef struct {
 static ParserKeyLink **parserList = NULL;
 static int registeredParsers = 0;
 
-static void logParser(void);
-
-static void log(char *msg) {
-  fprintf(stderr, ">> %s\n", msg);
-}
 
 void registerParser(const char *key, Parser parser) {
   void *ptr = realloc(parserList, (registeredParsers + 1) * sizeof((void *)0));
@@ -33,20 +28,12 @@ void registerParser(const char *key, Parser parser) {
   registeredParsers++;
 }
 
-static void logParser(void) {
-  ParserKeyLink *link = NULL;
-  for(int i = 0; i < registeredParsers; ++i) {
-    link = *(parserList + i);
-    fprintf(stderr, ">> Parser %d at %p ---> %s\n", i, link, link->key);
-  }
-}
 
 static Parser getParser(char *key) {
   ParserKeyLink *link = NULL;
   for(int i = 0; i < registeredParsers; ++i) {
     link = *(parserList + i);
     if(strcmp(link->key, key) == 0) {
-      fprintf(stderr, ">> Found Parser %s\n", link->key);
       return link->parser;
     }
   }
@@ -55,8 +42,6 @@ static Parser getParser(char *key) {
 }
 
 void parseConfig(const char *content, const void *config) {
-  log("STARTED");
-  logParser();
   struct TestConfiguration {
     char *name; int num; float fnum;
   };
@@ -75,11 +60,9 @@ void parseConfig(const char *content, const void *config) {
     Parser parser = getParser(field);
 
     if(!parser) {
-      fprintf(stderr, ">> NO Parser !!\n");
+      fprintf(stderr, ">> No Parser!\n");
     } else {
       parser(field, value, cfg);
     }
   }
-
-  log("FINISHED");
 }
