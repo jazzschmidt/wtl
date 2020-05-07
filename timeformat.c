a60cabaf75a5ff2332a35fb862a144d7e954975d
Carsten Schmidt <jazzschmidt@icloud.com>
2020-05-07 20:42:48 +0200

    Adds documentation

--- timeformat.c
+++ timeformat.c
@@ -1,3 +1,8 @@
+/* Time and Duration Functions
+ * ---------------------------
+ * Transforms time format strings, such as "08:00", into time_t values and vice
+ * versa.
+ */
 #import <stdlib.h>
 #import <stdio.h>
 #import <ctype.h>
@@ -5,10 +10,28 @@
 #import <string.h>
 #import "timeformat.h"
 
+/* --------------------- Global variables --------------------- */
+
+
+/* Splits hour and minute in the time format */
 static const char TIME_SEPARATOR = ':';
 
+
+/* --------------------- Prototypes --------------------- */
+
+
+/* Extracts hour and minute of the time string */
 static void parseTimeFormat(const char *format, int *hour, int *minute);
 
+
+/* --------------------- Implementation --------------------- */
+
+
+/* Function: isTimeFormat
+ * ----------------------
+ * Checks whether `format` is a time or duration format.
+ * Returns 1 if format matches, 0 otherwise.
+ */
 int isTimeFormat(const char *format) {
   if(strlen(format) > 5) return 0;
 
@@ -29,6 +52,11 @@ int isTimeFormat(const char *format) {
   return 1;
 }
 
+
+/* Function: timeFromFormat
+ * ------------------------
+ * Returns todays timestamp at the time of `format`.
+ */
 time_t timeFromFormat(const char *format) {
   int hour, minute;
   parseTimeFormat(format, &hour, &minute);
@@ -42,6 +70,10 @@ time_t timeFromFormat(const char *format) {
 }
 
 
+/* Function: durationFromFormat
+ * ----------------------------
+ * Returns the time of `format` as a duration in seconds.
+ */
 time_t durationFromFormat(const char *format) {
   int hour, minute;
   parseTimeFormat(format, &hour, &minute);
@@ -50,6 +82,10 @@ time_t durationFromFormat(const char *format) {
 }
 
 
+/* Function: formatTime
+ * --------------------
+ * Returns the textual representation of the given time.
+ */
 char *formatTime(time_t timestamp) {
   struct tm *local_time = localtime(&timestamp);
   char *format = NULL;
@@ -59,6 +95,14 @@ char *formatTime(time_t timestamp) {
   return format;
 }
 
+
+/* --------------------- Internal routines --------------------- */
+
+
+/* Function: parseTimeFormat
+ * -------------------------
+ * Extracts hour and minute of the time string.
+ */
 static void parseTimeFormat(const char *format, int *hour, int *minute) {
   int separator_pos;
 
